f216c4b6b776a30a62faf52e432bc2ee089b90fc
Adan Bms <abms19811@gmail.com>
2021-12-19 17:29:29 -0600

    Update Project to make a HTML report

--- src/test/java/runner/RunCukeTest.java
+++ src/test/java/runner/RunCukeTest.java
@@ -7,7 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/java/features",
-        glue = {"seleniumgluecode"}
+        glue = {"seleniumgluecode"},
+        plugin = {"com.vimalselvam.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"}
 )
 
 public class RunCukeTest {
